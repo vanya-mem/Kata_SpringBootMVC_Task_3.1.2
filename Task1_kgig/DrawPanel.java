52c0ef01ac45ce09b3cdde66f17efeed05330f9a
Nikita Vernigorov <nik.vernigorov.04@mail.ru>
2023-09-11 20:49:28 +0300

    Проверка

---
+++ Task1_kgig/DrawPanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+    public DrawPanel() {
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
+
---
+++ Task1_kgig/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+            MainWindow mw = new MainWindow();
+            mw.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            mw.setSize(800,800);
+            mw.setVisible(true);
+
+
+    }
+}
